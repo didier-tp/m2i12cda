7d0599d3f84f36ac820029d2f182b369a94a54dd
didier <didier@ici_ou_la.fr>
2019-08-20 16:37:31 +0200

    List, ArrayList

--- p1/src/main/java/com/mycompany/p1/Caddy.java
+++ p1/src/main/java/com/mycompany/p1/Caddy.java
@@ -1,34 +1,38 @@
 
 package com.mycompany.p1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Caddy {
     
-    //items est un tableau de 50 références
-    //sur des choses achetables.
-    private Buyable items[] = new Buyable[50];
-    int nbItems = 0;
+    //private List<Buyable> items = new ArrayList<Buyable>();
+    private List<Buyable> items = new ArrayList<>();//syntaxe simplifiée depuis jdk 1.7
     
     public void addItem(Buyable item){
-        if(this.nbItems<50){
-            this.items[nbItems]=item;
-            nbItems++;
-        }
+        items.add(item); //Collection.add(...) et List herite de Collection
     }
     
     public double totalPrice(){
        double prixTotal = 0.0 ;
-       for(int i=0; i<nbItems;i++){
-           prixTotal += items[i].getPrice();//polymorphisme
+       /*
+       for(int i=0; i<items.size();i++){
+           prixTotal += items.get(i).getPrice();//polymorphisme
+       }*/
+       
+       //for au sens forEach()
+       for(Buyable b : items){
+           prixTotal += b.getPrice();//polymorphisme
        }
        return prixTotal;
     }
 
     @Override
     public String toString() {
-        String contenu = "Caddy{ nbItems=" + nbItems + ",items=" ;
-         for(int i=0; i<nbItems;i++){
-             contenu += items[i].toString();
+        String contenu = "Caddy{ nbItems=" + items.size() + ",items=" ;
+          for(Buyable b : items){
+             contenu += b.toString();
          }
         return contenu + '}';
     
