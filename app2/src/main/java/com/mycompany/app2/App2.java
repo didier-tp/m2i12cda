564de1c63ebf26cba1c67a584506bc0e10eb44b4
didier <didier@ici_ou_la.fr>
2019-08-21 11:19:27 +0200

    tray/catch

--- app2/src/main/java/com/mycompany/app2/App2.java
+++ app2/src/main/java/com/mycompany/app2/App2.java
@@ -1,6 +1,7 @@
 
 package com.mycompany.app2;
 
+import java.awt.HeadlessException;
 import javax.swing.JOptionPane;
 
 /**
@@ -10,10 +11,15 @@ public class App2 {
     
     public static void main(String[] args) {
         
-        int a,b;
-        a = Integer.parseInt(JOptionPane.showInputDialog("a"));
-        b = Integer.parseInt(JOptionPane.showInputDialog("b"));
-        int c=a/b;
-        JOptionPane.showMessageDialog(null, "a/b="+c);
+        int a,b,c;
+        try {
+            a = Integer.parseInt(JOptionPane.showInputDialog("a"));
+            b = Integer.parseInt(JOptionPane.showInputDialog("b"));
+            c = a / b;
+            JOptionPane.showMessageDialog(null, "a/b=" + c);
+        }  catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "exception:" 
+                        + e.getMessage());
+        }
     }
 }
