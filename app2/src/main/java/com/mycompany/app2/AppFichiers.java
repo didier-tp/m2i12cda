f9275e3740e9fa36cebea5448626b86f2208b2d1
didier <didier@ici_ou_la.fr>
2019-08-23 11:53:24 +0200

    suite fichiers

--- app2/src/main/java/com/mycompany/app2/AppFichiers.java
+++ app2/src/main/java/com/mycompany/app2/AppFichiers.java
@@ -5,6 +5,11 @@
  */
 package com.mycompany.app2;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,11 +19,28 @@ import java.util.List;
  */
 public class AppFichiers {
     private List<Person> listePers = new ArrayList<>();
-    private void chargerFichier(String pathName){ 
+    
+    private void chargerFichier(String pathName){
+        try {
+            FileInputStream flux1 = new FileInputStream(pathName);
+            InputStreamReader flux2 = new InputStreamReader(flux1);
+            BufferedReader flux3 = new BufferedReader(flux2);
+            //flux3 basé sur flux2 basé sur flux1 (design patern "decorateur")
+            String ligne=null;
+            do {
+                ligne = flux3.readLine();
+                System.out.println("ligne lue=" + ligne);
+            }while(ligne != null);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     private void modifierListePersonnes(){ 
+        
     }
-    private void ecrireFichier(String pathName){ 
+    private void ecrireFichier(String pathName){
+        
     }
     public static void main(String[] args) {
         AppFichiers thisApp = new AppFichiers();
